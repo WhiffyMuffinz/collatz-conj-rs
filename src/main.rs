ec37498c305545f2558ae7b1b5282afa5ce9db96
Whiffy <dbene77@gmail.com>
2021-09-22 13:27:18 -0400

    fixed data generation

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let data = hasse(2 << 10, 2 << 12);
+    println!("Creating values...");
+    let data = hasse2(1, 2 << 12);
+    println!("done. \n");
 
     let file = create_file();
-    for arr in data.clone() {//might be able to remove the ".clone()"
+    println!("writing values to file...");
+    for arr in &data {
         write_to_file(&file, arr);
     }
     
-    for i in &data {
-        for j in i {
-            print!("{}, ", j);
-        }
-        println!("");
-    }
+    println!("done! \n")
 }
 
-fn hasse(from: u128, to: u128) -> Vec<Vec<u128>> {
+
+//the data that this creates is weird, goes 1,10,2,16,3,22...
+fn hasse(from: u128, to: u128, debug: bool) -> Vec<Vec<u128>> {
     //create a vec of vecs of u128, and a loop from "from" to "to", creating a vec of all the values. a tmp variable
     //passes through when the rules are applied. when the tmp == 1,
     //push the vec to the output and print the number of times the rules were applied to the tmp vaule bfore it settled on 1
@@ -33,11 +33,31 @@ fn hasse(from: u128, to: u128) -> Vec<Vec<u128>> {
             iter += 1;
         }
         out.push(tmp);
-        println!("{}->{}", i, iter);
+        if debug {
+            println!("{}->{}", i, iter);
+        }
     }
 
     return out;
 }
+
+fn hasse2 (from: u128, to: u128) -> Vec<Vec<u128>> {
+    let mut out = Vec::new();
+
+    for i in from..=to {
+        let mut j = i;
+        let mut tmp = vec![j];
+        while j > 1 {
+            j = apply_rules(j);
+            tmp.push(j);
+        }
+        out.push(tmp);
+    }
+
+    return out;
+}
+
+
 //applies the rules of the collatz conjecture
 fn apply_rules(n: u128) -> u128 {
     if n <= 1 {
@@ -51,9 +71,9 @@ fn apply_rules(n: u128) -> u128 {
 
 
 //writes one vec to file 
-fn write_to_file(mut dest: &File, data: Vec<u128>) {
+fn write_to_file(mut dest: &File, data: &Vec<u128>) {
     let mut n = 0;//tmp variable used fir coma addition
-    for i in &data {
+    for i in data {
         if n < data.len() - 1 {
                 write!(dest, "{}, ", i).unwrap();
         } else {
